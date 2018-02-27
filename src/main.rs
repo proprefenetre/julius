fc36484a31a7de877ff4307bd1faed89b7df3ec3
proprefenetre <proprefenetre@gmail.com>
2018-02-27 20:38:47 +0100

    encrypt/decrypt

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,40 @@
 extern crate clap;
 use clap::{Arg, App};
 
+use std::io;
+use std::io::prelude::*;
+use std::fs::File;
+
+
 /// The encryption can also be represented using modular arithmetic by first
 /// transforming the letters into numbers, according to the scheme, A → 0,
 /// B → 1, ..., Z → 25. 
+///
 
-fn encrypt(n: i32, text: &str) -> &str { // maybe define as a trait on String? 
+fn encrypt(shift: u8, text: &str) -> String {
     
     /// Encryption of a letter x by a shift n can be described mathematically as,
     /// *E~n~(x)* = *(x+n)* mod 26.
-
-    "implement later"
+    
+    let mut cipher: Vec<char> = vec![];
+    for c in text.chars() {
+        let case = if c.is_uppercase() { 'A' } else { 'a' } as u8;
+        if c.is_alphabetic() {
+            cipher.push((((c as u8 - case + shift) % 26) + case) as char);
+        } else {
+            cipher.push(c as char);
+        }
+    }
+    cipher.iter().collect::<String>()
 }
 
-fn decrypt(n: i32, text: &str) -> &str { // maybe define as a trait on String? 
+fn decrypt(shift: u8, text: &str) -> String {
+
     /// Decryption is performed similarly:
     /// *D~n~(x)* = *(x-n)* mod 26.
     
-    "implement later"
+    let shiftback = 26u8 - shift;
+    encrypt(shiftback, text)
 }
 
 /// (There are different definitions for the modulo operation. In the above, the
