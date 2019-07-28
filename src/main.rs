62d9181037c3a949590650dbd339d7b42ad6e341
proprefenetre <proprefenetre@gmail.com>
2019-07-28 15:14:02 +0200

    Code formatting

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 extern crate clap;
-use clap::{Arg, App};
+use clap::{App, Arg};
 
-use std::io;
-use std::io::prelude::*;
 use std::fs::File;
 use std::fs::OpenOptions;
+use std::io;
+use std::io::prelude::*;
 use std::path::Path;
 
 fn encrypt(shift: u8, text: &str) -> String {
@@ -37,42 +37,50 @@ fn file_or_str(inp: &str) -> String {
 }
 
 fn main() {
-   let matches = App::new("Caesar")
-                        .about("Encrypts text, Imperially")
-                        .version("1.0")
-                        .author("Niels Eigenraam <nielseigenraam@gmail.com>")
-                        .arg(Arg::with_name("encrypt")
-                                    .help("Encrypt text")
-                                    .short("e")
-                                    .long("encrypt")
-                                    .takes_value(true)
-                                    .value_name("TEXT or FILE")
-                                    .conflicts_with("decrypt"))
-                        .arg(Arg::with_name("decrypt")
-                                    .help("decrypt encrypted text")
-                                    .short("d")
-                                    .long("decrypt")
-                                    .takes_value(true)
-                                    .value_name("TEXT or FILE")
-                                    .conflicts_with("encrypt"))
-                        .arg(Arg::with_name("output")
-                                    .help("output file")
-                                    .short("o")
-                                    .long("output")
-                                    .takes_value(true)
-                                    .value_name("FILE"))
-                        .arg(Arg::with_name("shift")
-                                    .help("Encryption shift, default is 13")
-                                    .short("s")
-                                    .long("shift")
-                                    .takes_value(true)
-                                    .value_name("N"))
-                        .get_matches();
+    let matches = App::new("Caesar")
+        .about("Encrypts text, Imperially")
+        .version("1.0")
+        .author("Niels Eigenraam <nielseigenraam@gmail.com>")
+        .arg(
+            Arg::with_name("encrypt")
+                .help("Encrypt text")
+                .short("e")
+                .long("encrypt")
+                .takes_value(true)
+                .value_name("TEXT or FILE")
+                .conflicts_with("decrypt"),
+        )
+        .arg(
+            Arg::with_name("decrypt")
+                .help("decrypt encrypted text")
+                .short("d")
+                .long("decrypt")
+                .takes_value(true)
+                .value_name("TEXT or FILE")
+                .conflicts_with("encrypt"),
+        )
+        .arg(
+            Arg::with_name("output")
+                .help("output file")
+                .short("o")
+                .long("output")
+                .takes_value(true)
+                .value_name("FILE"),
+        )
+        .arg(
+            Arg::with_name("shift")
+                .help("Encryption shift, default is 13")
+                .short("s")
+                .long("shift")
+                .takes_value(true)
+                .value_name("N"),
+        )
+        .get_matches();
 
     let shift: u8 = matches.value_of("shift").unwrap_or("13").parse().unwrap();
 
     let value;
-    if matches.is_present("encrypt") { 
+    if matches.is_present("encrypt") {
         value = encrypt(shift, &file_or_str(matches.value_of("encrypt").unwrap()));
     } else if matches.is_present("decrypt") {
         value = decrypt(shift, &file_or_str(matches.value_of("decrypt").unwrap()));
@@ -81,15 +89,16 @@ fn main() {
     }
 
     match matches.value_of("output") {
-        Some(ref file) => { 
+        Some(ref file) => {
             let mut f = OpenOptions::new()
                 .write(true)
                 .create(true)
-                .open(file).unwrap();
-                f.write_fmt(format_args!("{}\n", value)).unwrap();
-        },
-        None => io::stdout().write_fmt(format_args!("> {}\n", value)).unwrap(),
+                .open(file)
+                .unwrap();
+            f.write_fmt(format_args!("{}\n", value)).unwrap();
+        }
+        None => io::stdout()
+            .write_fmt(format_args!("> {}\n", value))
+            .unwrap(),
     }
 }
-
-
